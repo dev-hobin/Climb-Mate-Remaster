0f13febb45987ead2b4b6f4fa502267d1c9baeaa
Dev-Hobin <devhobin@naver.com>
2021-03-27 01:21:44 +0900

    로그인 모달 이벤트 등록 기본 구조 완성

--- src/js/controller/HomeController.js
+++ src/js/controller/HomeController.js
@@ -10,7 +10,6 @@ const HomeController = class {
     this._modalView = new ModalView();
     this._alertView = new AlertView();
   }
-
   // 인터페이스
   init = () => {
     this._modalView //
@@ -20,11 +19,9 @@ const HomeController = class {
       .setup(document.querySelector('[data-top-menu]'))
       .on('@topMenuClick', event => this._onTopMenuClick(event.detail.name));
   };
-
   // 메서드
   _onTopMenuClick = menuName => {
     if (menuName === 'logOut') return console.log('로그아웃');
-    console.log(menuName, '모달 띄우기');
     this._modalView.showModal(menuName);
   };
 };
--- src/js/view/ModalView.js
+++ src/js/view/ModalView.js
@@ -24,12 +24,15 @@ const ModalView = class extends View {
     if (this._element.querySelector('[data-modal]')) {
       // 모달이 이미 있으면 안에 있는 내용만 변경
       this._changeContents(sort);
+      this._bindEvents(sort);
     } else {
       // 모달이 없으면 모달 생성
       this._makeModal(sort);
+      this._bindEvents(sort);
     }
+    // todo: 배경 클릭 이벤트 나중에 하자
     // 모달 종류에 따라 배경 클릭 이벤트 설정 (배경 눌렀을 때 모달 닫히게 할건지 안할건지)
-    this._setBackgroundEvent(sort);
+    // this._setBackgroundEvent(sort, this._element.querySelector('[data-modal]'));
   };
 
   // 메서드
@@ -70,12 +73,34 @@ const ModalView = class extends View {
   _changeContents = sort => {
     console.log(sort, '모달 컨텐츠 변경');
   };
+
+  // todo: 배경 클릭 이벤트 나중에 하자
   // 배경 클릭 이벤트 설정
-  _setBackgroundEvent = sort => {
-    console.log(sort, '배경 클릭 이벤트');
-  };
+  // _setBackgroundEvent = (sort, modalEl) => {};
   // 이벤트 바인딩
-  _bindEvents = () => {};
+  _bindEvents = sort => {
+    switch (true) {
+      case sort === this._modalSort.LOG_IN:
+        this._bindLogInEvents();
+        break;
+
+      case sort === this._modalSort.SIGN_UP:
+        this._bindSignUpEvents();
+        break;
+
+      case sort === this._modalSort.QUESTION:
+        this._bindQuestionEvents();
+        break;
+      case sort === this._modalSort.AGREEMENT:
+        this._bindAgreementEvents();
+        break;
+      case sort === this._modalSort.USER_REGISTER:
+        this._bindUserRegisterEvents();
+        break;
+      default:
+        throw `${tag} 이벤트 등록에 실패했습니다`;
+    }
+  };
   // 모달 엘리먼트 추가
   _addModalElement = () => {
     // 모달 엘리먼트 생성
@@ -91,12 +116,12 @@ const ModalView = class extends View {
   // 로그인 모달 html 생성
   _getLogInHtml = () => {
     return `<div class="modal__contents" data-contents="logIn">
-        <header class="modal__header">
+        <header class="modal__header" data-header>
           <span class="modal__title">로그인</span>
-          <button class="icon-btn icon-btn-times modal__close-btn"></button>
+          <button class="icon-btn icon-btn-times modal__close-btn" data-close-btn></button>
         </header>
         <div class="modal__body">
-          <button class="kakao-btn modal__kakao-btn">
+          <button class="kakao-btn modal__kakao-btn" data-kakao-btn>
             <img src="assets/icon/kakao/kakao-symbol.png" alt="카카오 심볼" class="kakao-btn__symbol-img" />
             <span class="kakao-btn__text">카카오 로그인</span>
           </button>
@@ -107,24 +132,24 @@ const ModalView = class extends View {
           <div class="margin-bottom-container">
             <div class="input-box">
               <div class="icon icon-email input-box__icon"></div>
-              <input type="email" class="input-box__input" placeholder="이메일을 입력해주세요" />
+              <input type="email" class="input-box__input" placeholder="이메일을 입력해주세요" data-input="email"/>
             </div>
           </div>
           <div class="input-box modal__input-box--has-content">
             <div class="icon icon-password input-box__icon"></div>
-            <input type="password" class="input-box__input" placeholder="비밀번호를 입력해주세요" />
+            <input type="password" class="input-box__input" placeholder="비밀번호를 입력해주세요" data-input="password"/>
           </div>
           <div class="flex-end-container modal__btn-container">
-            <button class="modal__sub-btn">비밀번호 찾기</button>
+            <button class="modal__sub-btn" data-sub-btn="temporalPassword">비밀번호 찾기</button>
           </div>
           <div class="flex-full-container">
-            <button class="modal__main-btn">로그인</button>
+            <button class="modal__main-btn" data-main-btn="logIn">로그인</button>
           </div>
         </div>
         <footer class="modal__footer">
           <div class="flex-container">
             <p class="modal__footer-text">Climb:Mate 계정이 없으신가요?</p>
-            <button class="modal__sub-btn">회원가입</button>
+            <button class="modal__sub-btn" data-sub-btn="signUp">회원가입</button>
           </div>
         </footer>
       </div>`;
@@ -132,7 +157,7 @@ const ModalView = class extends View {
   // 회원가입 모달 html 생성
   _getSignUpHtml = () => {
     return `<div class="modal__contents" data-contents="signUp">
-        <header class="modal__header">
+        <header class="modal__header" data-header>
           <span class="modal__title">회원 가입</span>
           <button class="icon-btn icon-btn-times modal__close-btn"></button>
         </header>
@@ -160,7 +185,7 @@ const ModalView = class extends View {
   // 문의하기 모달 html 생성
   _getQuestionHtml = () => {
     return `<div class="modal__contents" data-contents="question">
-        <header class="modal__header">
+        <header class="modal__header" data-header>
           <span class="modal__title">문의하기</span>
           <button class="icon-btn icon-btn-times modal__close-btn"></button>
         </header>
@@ -190,7 +215,7 @@ const ModalView = class extends View {
   // 약관동의 모달 html 생성
   _getAgreementHtml = () => {
     return `<div class="modal__contents" data-contents="agreement">
-        <header class="modal__header">
+        <header class="modal__header" data-header>
           <span class="modal__title">회원가입</span>
           <button class="icon-btn icon-btn-times modal__close-btn"></button>
         </header>
@@ -247,7 +272,7 @@ const ModalView = class extends View {
   // 회원 정보 등록 모달 html 생성
   _getRegisterUserInfoHtml = () => {
     return `<div class="modal__contents" data-contents="user-register">
-        <header class="modal__header">
+        <header class="modal__header" data-header>
           <span class="modal__title">회원정보 등록</span>
           <button class="icon-btn icon-btn-times modal__close-btn"></button>
         </header>
@@ -292,6 +317,49 @@ const ModalView = class extends View {
         </div>
       </div>`;
   };
+
+  // 로그인 모달 이벤트 등록
+  _bindLogInEvents = () => {
+    const contents = document.querySelector('[data-contents="logIn"]');
+
+    const closeBtn = contents.querySelector('[data-close-btn]');
+    const kakaoLogInBtn = contents.querySelector('[data-kakao-btn]');
+    const emailInput = contents.querySelector('[data-input="email"]');
+    const passwordInput = contents.querySelector('[data-input="password"]');
+    const temporalPasswordModalBtn = contents.querySelector('[data-sub-btn="temporalPassword"]');
+    const signUpModalBtn = contents.querySelector('[data-sub-btn="signUp"]');
+    const logInBtn = contents.querySelector('[data-main-btn="logIn"]');
+
+    closeBtn.addEventListener('click', this._removeModal);
+    kakaoLogInBtn.addEventListener('click', () => console.log('카카오 로그인 버튼 클릭'));
+    emailInput.addEventListener('keyup', event => {
+      if (event.key !== 'Enter') return;
+      // 엔터를 눌렀을 경우
+      const email = emailInput.value;
+      const password = passwordInput.value;
+      // 입력한 이메일, 비밀번호로 로그인 요청
+      this.emit('@logIn', { email, password });
+    });
+    temporalPasswordModalBtn.addEventListener('click', () => console.log('임시 비밀번호 모달 띄우기'));
+    signUpModalBtn.addEventListener('click', () => console.log('회원가입 모달 띄우기'));
+    logInBtn.addEventListener('click', () => {
+      const email = emailInput.value;
+      const password = passwordInput.value;
+      // 입력한 이메일, 비밀번호로 로그인 요청
+      this.emit('@logIn', { email, password });
+    });
+  };
+  // 회원가입 모달 이벤트 등록
+  _bindSignUpEvents = () => {};
+  // 문의하기 모달 이벤트 등록
+  _bindQuestionEvents = () => {};
+  // 약관 동의 모달 이벤트 등록
+  _bindAgreementEvents = () => {};
+  // 회원정보 등록 모달 이벤트 등록
+  _bindUserRegisterEvents = () => {};
+
+  // 모달 제거
+  _removeModal = () => document.querySelector('[data-modal]').remove();
 };
 
 export default ModalView;
